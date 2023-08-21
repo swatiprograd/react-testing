3b8a5be1a475dd244fdee9c8bafcb6283136908f
Swati Pandey <swati.p@prograd.org>
2023-08-21 17:18:21 +0530

    Edit App Test

--- react-testing/src/App.test.js
+++ react-testing/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders "This is a React Testing Application" text in the document', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/This is a React Testing Application/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Login Component in the document', () => {
+  const component=render(<App />);
+  const childElement = component.getByLabelText("Email");
+  expect(childElement).toBeTruthy();
 });
